f4c3a6ecfd721234d04e3daacf65b4c519b1d8a4
Nurullah CAMCI <59570845+ncamci@users.noreply.github.com>
2024-06-10 16:40:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,43 +37,56 @@ app.post("/webhook",(req,res)=>{ //i want some
 
     console.log(JSON.stringify(body_param,null,2));
 
-    if(body_param.object){
-        console.log("inside body param");
-        if(body_param.entry && 
-            body_param.entry[0].changes && 
-            body_param.entry[0].changes[0].value.messages && 
-            body_param.entry[0].changes[0].value.messages[0]  
-            ){
-               let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
-               let from = body_param.entry[0].changes[0].value.messages[0].from; 
-               let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+    if (body_param.object) {
+    console.log("inside body param");
+    if (
+        body_param.entry && 
+        body_param.entry[0].changes && 
+        body_param.entry[0].changes[0].value.messages && 
+        body_param.entry[0].changes[0].value.messages[0]
+    ) {
+        let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
+        let from = body_param.entry[0].changes[0].value.messages[0].from;
+        let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body.toLowerCase();
 
-               console.log("phone number "+phon_no_id);
-               console.log("from "+from);
-               console.log("boady param "+msg_body);
+        console.log("phone number " + phon_no_id);
+        console.log("from " + from);
+        console.log("body param " + msg_body);
 
-               axios({
-                   method:"POST",
-                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
-                   data:{
-                       messaging_product:"whatsapp",
-                       to:from,
-                       text:{
-                           body:"Merhaba, Ben YumChatbot, size nasıl yardımcı olabilirim?" // +msg_body
-                       }
-                   },
-                   headers:{
-                       "Content-Type":"application/json"
-                   }
+        let responseMessage;
 
-               });
+        // Define responses based on the message body
+        if (msg_body === "hi") {
+            responseMessage = "How can I help you today?";
+        } else if (msg_body === "bye") {
+            responseMessage = "Goodbye! Have a great day!";
+        } else if (msg_body === "thanks") {
+            responseMessage = "You're welcome!";
+        } else {
+            responseMessage = "Sorry, I didn't understand that. Can you please rephrase?";
+        }
 
-               res.sendStatus(200);
-            }else{
-                res.sendStatus(404);
+        axios({
+            method: "POST",
+            url: "https://graph.facebook.com/v13.0/" + phon_no_id + "/messages?access_token=" + token,
+            data: {
+                messaging_product: "whatsapp",
+                to: from,
+                text: {
+                    body: responseMessage
+                }
+            },
+            headers: {
+                "Content-Type": "application/json"
             }
+        });
 
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
     }
+}
+
 
 });
 
